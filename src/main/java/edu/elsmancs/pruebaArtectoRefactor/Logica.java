a40cc4813f34575de42645bcf1b231c108f70b94
sr_Adrover <sadrover@cifpfbmoll.eu>
2020-08-18 12:35:55 +0200

    Feat: Implementar código para generar String []

--- src/main/java/edu/elsmancs/pruebaArtectoRefactor/Logica.java
+++ src/main/java/edu/elsmancs/pruebaArtectoRefactor/Logica.java
@@ -2,9 +2,15 @@ package edu.elsmancs.pruebaArtectoRefactor;
 
 public class Logica {
 	private String loren = "";
+	private String [] arrLoren = null;
 	
 	public Logica (String loren) {
 		this.loren = loren.toLowerCase();
+		this.generarLista();
+	}
+	
+	public String[] getArr() {
+		return arrLoren;
 	}
 	
 	public int contarPalabras() {
@@ -16,5 +22,11 @@ public class Logica {
 	
 	public int contarParrafos() {
 		return (int)this.loren.chars().filter(c -> c == '\n').count();
+	}
+	
+	private void generarLista() {
+		StringBuilder texto = new StringBuilder();
+		loren.chars().mapToObj(c -> (char)c).filter(c -> c != ',' && c != '.' && c != '\n' && c != ';' ).forEach(c -> texto.append(c));
+		arrLoren = texto.toString().split(" ");
 	}
 }
