a6540ac481c1b27c0a2d803011e294dbec4941ae
sr_Adrover <sadrover@cifpfbmoll.eu>
2020-08-18 12:31:04 +0200

    Feat: Métodos contar frases y contar párrafos

--- src/main/java/edu/elsmancs/pruebaArtectoRefactor/Logica.java
+++ src/main/java/edu/elsmancs/pruebaArtectoRefactor/Logica.java
@@ -10,4 +10,11 @@ public class Logica {
 	public int contarPalabras() {
 		return (int)this.loren.chars().filter(c -> c == ' ').count();
 	}
+	public int contarFrases() {
+		return (int)this.loren.chars().filter(c -> c == '.').count();
+	}
+	
+	public int contarParrafos() {
+		return (int)this.loren.chars().filter(c -> c == '\n').count();
+	}
 }
--- src/test/java/edu/elsmancs/pruebaArtectoRefactor/AppTest.java
+++ src/test/java/edu/elsmancs/pruebaArtectoRefactor/AppTest.java
@@ -4,7 +4,6 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
-import edu.elsmancs.ArtecoRefactorizado.Logica;
 
 
 /**
