baefbeda33f1150611602646703f2bc84b891769
sr_Adrover <sadrover@cifpfbmoll.eu>
2020-08-18 12:29:31 +0200

    Test: Implentar Tests de contar frases y párrafos

--- src/test/java/edu/elsmancs/pruebaArtectoRefactor/AppTest.java
+++ src/test/java/edu/elsmancs/pruebaArtectoRefactor/AppTest.java
@@ -4,6 +4,8 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
+import edu.elsmancs.ArtecoRefactorizado.Logica;
+
 
 /**
  * Unit test for simple App.
@@ -19,5 +21,24 @@ public class AppTest
     			     + "Yo estoy bien. Y tú? \n";
     	Logica logica = new Logica(texto);
     	assertTrue(8 == logica.contarPalabras());
+    }
+	
+	@Test
+    public void contarFrasesTest() {
+    	String texto = "Hola amigo. \n"
+    			     + "Esto es un; texto. \n"
+    			     + "Debe, tener, 3 frases. \n";
+    	Logica logica = new Logica(texto);
+    	assertTrue(3 == logica.contarFrases());
+    }
+    
+    @Test
+    public void contarParrafosTest() {
+    	String texto = "Hola amigo. \n"
+    			     + "Arteco mola. \n"
+    			     + "Esto es un; texto. \n"
+    			     + "Debe, tener, 3 frases. \n";
+    	Logica logica = new Logica(texto);
+    	assertTrue(4 == logica.contarParrafos());
     }
 }
