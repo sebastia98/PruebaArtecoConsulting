d413a889b087fd8775e9fe342e8f537ac5bd8a29
sr_Adrover <sadrover@cifpfbmoll.eu>
2020-08-18 13:02:34 +0200

    Docs: Añadir comentarios

--- src/main/java/edu/elsmancs/pruebaArtectoRefactor/Logica.java
+++ src/main/java/edu/elsmancs/pruebaArtectoRefactor/Logica.java
@@ -26,7 +26,10 @@ public class Logica {
 	public String[] getArr() {
 		return arrLoren;
 	}
-	
+	/*
+	 * Métodos para contar palabras, frases y párrafos. En todos transformo el String en un Stream de chars 
+	 * y filtro los elementos que más me convienen para contarlos.
+	 */
 	public int contarPalabras() {
 		return (int)this.loren.chars().filter(c -> c == ' ').count();
 	}
@@ -37,13 +40,20 @@ public class Logica {
 	public int contarParrafos() {
 		return (int)this.loren.chars().filter(c -> c == '\n').count();
 	}
-	
+	/*
+	 * Método que genera una lista de Strings. Filtro todo aquello que no sean signos de puntuación excepto espacios. 
+	 * Hago uso de la Clase StringBuilder que me permite añadir los chars más cómodamente. Luego lo paso a String y 
+	 * creo un String[] con el método split.
+	 */
 	private void generarLista() {
 		StringBuilder texto = new StringBuilder();
 		loren.chars().mapToObj(c -> (char)c).filter(c -> c != ',' && c != '.' && c != '\n' && c != ';' ).forEach(c -> texto.append(c));
 		arrLoren = texto.toString().split(" ");
 	}
-	
+	/*
+	 * Itero cada palabra de la lista y la convierto en StringBuilder, ya que con su método reverse me permite 
+	 * comprobar si es igual a su forma original.
+	 */
 	public ArrayList<String> listarPalindromos() {
 		ArrayList<String> listaPal = new ArrayList<String>();
 		for (String w : arrLoren) {
@@ -52,21 +62,32 @@ public class Logica {
 		}
 		return listaPal;
 	}
-	
+	/*
+	 * Se le pasa un Object[] y devuelve un mapa ordenado de los cinco elementos mas repetidos de ese array.
+	 */
 	private Map<String, Integer> generarMapa(Object [] arr) {
 		
 		Map<String, Integer> tabla = new HashMap<String, Integer>();
-		
+		/*
+		 * Itero cada Objeto del array que le paso y la filtro para poder contarla, 
+		 * después añado el Objeto casteado a String y su cantidad en un Mapa en caso de que este no lo contenga.
+		 */
 		for (Object palabra : arr) {
 			int suma = (int)Stream.of(arr).filter(w -> w.equals(palabra)).count();
 			if (!tabla.containsKey(palabra)) tabla.put((String) palabra, suma);
 		}
-		
+		/*
+		 * Luego lo ordeno en función del valor de las entradas del mapa, devolviendo solo 5 entradas y 
+		 * coleccionadas en un LinkedHashMap.
+		 */
 		return tabla.entrySet().stream()
 							   .sorted(Map.Entry.<String, Integer>comparingByValue().reversed()).limit(5)
 							   .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new));
 	}
-	
+	/*
+	 * Método que genera Strings de dos palabras de forma ordenada y secuencial 
+	 * con todas las palabras de arrLoren.
+	 */
 	private ArrayList<String> generarTuplas() {
 		ArrayList<String> listaTuplas = new ArrayList<String>();
 		for (int i = 0; i < arrLoren.length; i++) {
@@ -78,7 +99,10 @@ public class Logica {
 		}
 		return listaTuplas;
 	}
-	
+	/*
+	 * Estos métodos "cuentan" las palabras y las tuplas llamando al método generarMapa y pasándole por 
+	 * parámetro la estructura de datos que necesite para realizar su función.
+	 */
 	public Map<String, Integer> contarRepetidas(){
 		return generarMapa(arrLoren);
 	}
